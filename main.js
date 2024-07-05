2e256ad8daaa9c669597457726271bbfc6c09bd1
Monireh Eshghinezhad <monire.eshghi@gmail.com>
2024-07-05 14:57:35 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+const express = require("express")
+const dotenv= require("dotenv");
+const SwaggerConfig = require("./src/config/swagger.config");
+const mainRouter = require("./src/app.routes");
+const NotFoundHandler = require("./src/common/exception/not-found.handler");
+const AllExceptionHandler = require("./src/common/exception/all-exception.handler");
+dotenv.config();
+
+async function main(){
+   const app = express();
+   const port = process.env.PORT;
+   require("./src/config/mongoose.config");
+   app.use(express.json());
+   app.use(express.urlencoded({extended:true}));
+
+   SwaggerConfig(app);
+   app.use(mainRouter);
+   NotFoundHandler(app);
+   AllExceptionHandler(app);
+
+
+   app.listen(port, () => {
+      console.log(`server is runing: http://localhost:${port}`);
+   })
+}
+main();
+
+
+
